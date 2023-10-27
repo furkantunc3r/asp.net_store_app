3508f2fad1708c4f2efd467d720755bb9c6660c5
furkantunc3r <furkan_tuncer@yahoo.com>
2023-10-27 10:10:08 +0300

    CRUD

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -1,7 +1,13 @@
-﻿namespace Entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
 public class Product
 {
     public int ProductId { get; set; }
+    
+    [Required(ErrorMessage = "ProductName is required.")]
     public string? ProductName { get; set; }
+    
+    [Required(ErrorMessage = "Price is required.")]
     public decimal Price { get; set; }
 }
--- Services/Contracts/IProductService.cs
+++ Services/Contracts/IProductService.cs
@@ -5,6 +5,11 @@ namespace Services.Contracts
     public interface IProductService
     {
         IEnumerable<Product> GetAllProducts(bool trackChanges);
+        
         Product? GetOneProduct(int id, bool trackChanges);
+
+        void CreateProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
     }
 }
